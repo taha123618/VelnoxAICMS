50f7c678b10395fdd154be17464e019c5100f1f8
taha123618 <tahaahmedanees2@gmail.com>
2026-07-18 17:27:47 +0500

    Update admin sidebar navigation

--- Modules/Dashboard/resources/layouts/AdminLayout.vue
+++ Modules/Dashboard/resources/layouts/AdminLayout.vue
@@ -217,6 +217,45 @@ const mainNavItems = computed<NavigationMenuItem[]>(() => [
             },
         ],
     },
+    {
+        label: 'Marketplace',
+        icon: 'ph:storefront',
+        active: route().current('marketplace.*'),
+        onSelect: () => router.visit(route('marketplace.index')),
+    },
+    {
+        label: 'Headless CMS',
+        type: 'label',
+        icon: 'ph:database',
+        trailingIcon: 'ph:minus',
+        open: true,
+        children: [
+            {
+                label: 'Collections',
+                active: route().current('admin.collections*'),
+                onSelect: () => router.visit(route('admin.collections.index')),
+            },
+        ],
+    },
+    {
+        label: 'Platform Settings',
+        type: 'label',
+        icon: 'ph:gear',
+        trailingIcon: 'ph:minus',
+        open: true,
+        children: [
+            {
+                label: 'Languages',
+                active: route().current('admin.languages*'),
+                onSelect: () => router.visit(route('admin.languages.index')),
+            },
+            {
+                label: 'Webhooks',
+                active: route().current('admin.webhooks*'),
+                onSelect: () => router.visit(route('admin.webhooks.index')),
+            },
+        ],
+    },
 ]);
 
 const userMenuItems = ref<DropdownMenuItem[]>([
